419d296e8be0636a98327aa103a6efccd9c5319f
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2024-04-08 12:13:02 +0400

    chapter 2 exercise and bonus (updated)

--- Chapter-6-Javascript/pertol.js
+++ Chapter-6-Javascript/pertol.js
@@ -1,12 +1,12 @@
-window.onload=()=>{const inputs=document.querySelectorAll("input");
-inputs.forEach(input =>{
-    input.addEventListener('change',calculate)
-})
-}
-function calculate(){const Petrol_Price=document.querySelector("#Petrol_price").value;
-const liters=document.querySelector("#liters").value;
-if("!Petrol_Price||liters")returns;
-document.querySelector("#totalAmount").innerHTML= Petrol_Price*liters;}
+window.onload=()=>{const inputs=document.querySelectorAll("inputs");
+    inputs.forEach(input =>{
+        input.addEventListener("change","calculate")
+    })
+    }
+    function calculate(){const Petrol_Price=document.querySelector("#Petrol_Price").value;
+    const liters=document.querySelector("#liters").value;
+    if(!Petrol_Price||!liters)returns;
+    document.querySelector("#totalAmount").innerHTML= Petrol_Price*liters;}
 
     
 
